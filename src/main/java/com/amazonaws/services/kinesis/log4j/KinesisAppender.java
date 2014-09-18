c1eb1290a23653bb9c42828fe001799d5faaf95c
Andrew Elliott <andrewcaelliott@gmail.com>
2014-09-18 14:03:11 +0100

    #Added Rev Sys fork comment

--- src/main/java/com/amazonaws/services/kinesis/log4j/KinesisAppender.java
+++ src/main/java/com/amazonaws/services/kinesis/log4j/KinesisAppender.java
@@ -12,6 +12,10 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  ******************************************************************************/
+/* 
+ * Forked, Revolutionary Systems.
+ * 
+ ******************************************************************************/
 package com.amazonaws.services.kinesis.log4j;
 
 import java.nio.ByteBuffer;
