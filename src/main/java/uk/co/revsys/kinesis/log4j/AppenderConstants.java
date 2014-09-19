9007cb6ad6a49a1e6b58c6f9ce6c1446483a3804
Andrew Elliott <andrewcaelliott@gmail.com>
2014-09-19 09:11:27 +0100

    #Working version.

--- src/main/java/uk/co/revsys/kinesis/log4j/AppenderConstants.java
+++ src/main/java/uk/co/revsys/kinesis/log4j/AppenderConstants.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  ******************************************************************************/
-package com.amazonaws.services.kinesis.log4j;
+package uk.co.revsys.kinesis.log4j;
 
 /**
  * Contains constants and default configuration values for the appender
--- src/main/java/uk/co/revsys/kinesis/log4j/FilePublisher.java
+++ src/main/java/uk/co/revsys/kinesis/log4j/FilePublisher.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  ******************************************************************************/
-package com.amazonaws.services.kinesis.log4j;
+package uk.co.revsys.kinesis.log4j;
 
 import java.io.BufferedReader;
 import java.io.File;
